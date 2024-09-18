47f52df7e7071d195555fede861b5b329e54365b
Ann Maria Jaison <annkochu1234@gmail.com>
2024-09-19 00:24:23 +0530

    Initial upload of project files.

---
+++ Annmariajaison_2024_sjcet.py
@@ -0,0 +1,48 @@
+from turtle import *
+
+speed(0)
+pensize(2)
+
+goto(10, -1.5)  # Move to the center
+pendown()
+
+# Draw the new circle
+color("dark slate blue")  # Color of the new circle
+begin_fill()
+
+circle(127)  # Size of the new circle
+
+end_fill()
+
+# Draw outer pattern of circles
+goto(0, 0)
+color("orange", "lime green")
+begin_fill()
+
+for i in range(40):
+    
+    circle(40)
+    forward(20)
+    left(9)
+
+end_fill()
+
+# Move the turtle to a new position for the star-like pattern
+penup()
+goto(2, 80)  # Center position
+pendown()
+
+# Draw star-like pattern with smaller circles
+color("mediumvioletred", "goldenrod")
+begin_fill()
+
+for i in range(20):
+    circle(20)  # Smaller size
+    penup()
+    forward(15)  # Adjust forward distance
+    pendown()
+    left(18)
+
+end_fill()
+
+done()
